d397bd4d14128d5bebafd8b166fa5b89ba00a158
gakuzzzz <gaku_github@t2v.jp>
2015-02-09 22:22:19 +0900

    format

--- src/main/scala/scalikejdbc/free/Interpreter.scala
+++ src/main/scala/scalikejdbc/free/Interpreter.scala
@@ -8,11 +8,11 @@ import scalaz.~>
 
 object Interpreter extends (Query ~> Executor) {
   def apply[A](c: Query[A]) = c match {
-    case GetSeq(sql) => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
-    case GetFirst(sql) => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
-    case GetSingle(sql) => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
-    case Execute(sql) => exec(sql.apply()(_))
-    case Update(sql) => exec(sql.apply()(_))
+    case GetSeq(sql)                 => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
+    case GetFirst(sql)               => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
+    case GetSingle(sql)              => exec(s => sql.apply()(session = s, NoConnectionPoolContext, null))   // TODO:
+    case Execute(sql)                => exec(sql.apply()(_))
+    case Update(sql)                 => exec(sql.apply()(_))
     case UpdateWithGeneratedKey(sql) => exec(sql.apply()(_))
   }
 }
